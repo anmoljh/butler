4d7e05825067a5861fa6c0af987e79490fe6eeb1
Anmol Hemrom <hemrom@ebi.ac.uk>
2018-05-09 10:33:56 +0100

    Fixed outputs.tf syntax error

--- examples/deployment/openstack/large-cluster/outputs.tf
+++ examples/deployment/openstack/large-cluster/outputs.tf
@@ -1,7 +1,6 @@
 output "salt-master" {
   value = "${openstack_compute_instance_v2.salt-master.access_ip_v4}"
 }
-
 output "db-server" {
  value = "${openstack_compute_instance_v2.db-server.access_ip_v4}"
 }
@@ -11,12 +10,6 @@ output "tracker" {
 output "job-queue" {
  value = "${openstack_compute_instance_v2.job-queue.access_ip_v4}"
 }
-output "worker-0" {
- value = "${openstack_compute_instance_v2.worker.0.access_ip_v4}"
-}
-output "worker-1" {
- value = "${openstack_compute_instance_v2.worker.1.access_ip_v4}"
-}
-output "worker-2" {
- value = "${openstack_compute_instance_v2.worker.1.access_ip_v4}"
+output "worker" {
+ value = "${join(",",openstack_compute_instance_v2.worker.*.access_ip_v4)}"
 }
