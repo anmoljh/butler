fea59144f62cd89730dd7e89e31ebc2ae13ce541
Anmol Hemrom <hemrom@ebi.ac.uk>
2018-05-09 09:41:54 +0100

    Fixed syntax error for uninstalling tornado

--- examples/deployment/openstack/large-cluster/saltmaster.tf
+++ examples/deployment/openstack/large-cluster/saltmaster.tf
@@ -43,7 +43,7 @@ resource "openstack_compute_instance_v2" "salt-master" {
 		     "sudo mv -f /home/centos/master /etc/salt/master",       
 		     "sudo yum install salt-minion -y",
 		     "sudo yum install python-pip -y",
-		     "sudo pip uninstall tornado",
+		     "sudo pip uninstall tornado -y",
 		     "sudo pip install tornado",
 		     "sudo yum install GitPython -y",
 		     "sudo service salt-master start",
